12edab0981cf4cf3e4925f59ce6eaebfd9c06614
Danielle Ballester <danielleballester@gmail.com>
2026-03-02 16:21:23 -0300

    chore: clean gitignore patterns and remove unused dirs from cache

--- scripts/bat/executar_engine.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Atalho rapido para executar SmartCorr_Engine.py
-call "%~dp0executar_com_dominio.bat" src\SmartCorr_Engine.py
--- tests/teste_banco.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Atalho rapido para executar teste_leitura_banco.py
-call "%~dp0executar_com_dominio.bat" teste_leitura_banco.py
